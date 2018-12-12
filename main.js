5ab8b91d5436a27b8904c4b3fe2f7944fca7b715
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2018-12-12 11:03:30 +1100

    Create the initialization function

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 function startGame() {
-
+  myGameArea.start();
 }
+
+var myGameArea = {
+  canvas : document.createElement("canvas"),
+  start : function() {
+    this.canvas.width = 480;
+    this.canvas.height = 270;
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.frameNo = 0;
+  },
+  clear : function() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+}
+
