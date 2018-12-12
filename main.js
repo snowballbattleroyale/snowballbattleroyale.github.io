2707b2b836274fe9244cf45c1b602ca932475f24
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2018-12-12 11:28:01 +1100

    Update main.js

--- main.js
+++ main.js
@@ -1,31 +1,26 @@
 var myGamePiece;
 
 function startGame() {
-  myGamePiece = new component(30, 30, "red", 10, 120);
-  myGameArea.start();
+    myGameArea.start();
+    myGamePiece = new component(30, 30, "red", 10, 120);
 }
 
 var myGameArea = {
-  canvas : document.createElement("canvas"),
-  start : function() {
-    this.canvas.width = window.innerWidth - 10;
-    this.canvas.height = window.innerHeight - 20;
-    this.context = this.canvas.getContext("2d");
-    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-    this.frameNo = 0;
-  },
-  clear : function() {
-    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  }
+    canvas : document.createElement("canvas"),
+    start : function() {
+        this.canvas.width = window.innerWidth-10;
+        this.canvas.height = window.innerHeight-20;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    }
 }
 
 function component(width, height, color, x, y) {
-  this.width = width;
-  this.height = height;
-  this.x = x;
-  this.y = y; 
-  ctx = myGameArea.context;
-  ctx.fillStyle = color;
-  ctx.fillRect(this.x, this.y, this.width, this.height);
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;    
+    ctx = myGameArea.context;
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
 }
-
