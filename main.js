442c8a9bf9c2b1525c15727564fb1bc4ea2ab28a
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2018-12-13 10:40:36 +1100

    Update main.js

--- main.js
+++ main.js
@@ -124,7 +124,7 @@ function moveSnowball() {
     else {
         snowball.x = player.x;
         snowball.y = player.y;
-        pointto(snowball.x, snowball.y, gameArea.x, gameArea.y, 80, snowball);
+        pointto(snowball.x, snowball.y, gameArea.x, gameArea.y, 75, snowball);
     }
 }
 
