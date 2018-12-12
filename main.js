358f71e9a9fedd77e810376bd3a8bf58c291db54
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2018-12-12 18:56:39 +1100

    Do som more rotation

--- main.js
+++ main.js
@@ -40,9 +40,12 @@ function component(width, height, color, x, y) {
     this.y = y;    
     this.update = function() {
         ctx = myGameArea.context;
+        ctx.save();
+        ctx.translate(this.x, this.y); 
         ctx.rotate(this.angle);
         ctx.fillStyle = color;
-        ctx.fillRect(this.x, this.y, this.width, this.height);
+        ctx.fillRect(this.width / -2, this.height / -2, this.width, this.height); 
+        ctx.restore(); 
     }
 }
 
