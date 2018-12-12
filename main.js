1280307580cb46b8daa7b2f8bf832b3f3f2b7940
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2018-12-12 11:20:26 +1100

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ var myGameArea = {
   canvas : document.createElement("canvas"),
   start : function() {
     this.canvas.width = window.innerWidth - 10;
-    this.canvas.height = window.innerHeight - 10;
+    this.canvas.height = window.innerHeight - 20;
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     this.frameNo = 0;
