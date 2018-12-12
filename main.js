d7ee2d963ddfafe54267a64cdb435a40f4a0f076
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2018-12-12 11:32:35 +1100

    Make is so you can do the update dance

--- main.js
+++ main.js
@@ -12,6 +12,9 @@ var myGameArea = {
         this.canvas.height = window.innerHeight-20;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    },
+    clear : function() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 
@@ -20,7 +23,14 @@ function component(width, height, color, x, y) {
     this.height = height;
     this.x = x;
     this.y = y;    
+    this.update = function(){
     ctx = myGameArea.context;
     ctx.fillStyle = color;
     ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+function updateGameArea() {
+  myGameArea.clear();
+  myGamePiece.update();
 }
