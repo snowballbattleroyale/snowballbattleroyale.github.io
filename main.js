a8e288eaf4363b0f951e8ca420967854c7f3cd68
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2018-12-12 11:17:26 +1100

    Change size of cavs

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ function startGame() {
 var myGameArea = {
   canvas : document.createElement("canvas"),
   start : function() {
-    this.canvas.width = 480;
-    this.canvas.height = 270;
+    this.canvas.width = window.innerWidth;
+    this.canvas.height = window.innerHeight;
     this.context = this.canvas.getContext("2d");
     document.body.insertBefore(this.canvas, document.body.childNodes[0]);
     this.frameNo = 0;
