a21f96e3324257342d3d466dc61c416c6e7ddecc
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2018-12-12 11:46:47 +1100

    Update main.js

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ var myGameArea = {
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
         window.addEventListener('mousedown', function (e) {
-            myGameArea.x = e.pageX-10;
-            myGameArea.y = e.pageY-10;
+            myGameArea.x = e.pageX-30;
+            myGameArea.y = e.pageY-30;
         })
     }, 
     clear : function(){
