1eef77d23c54a5a702eb64a1f8e1f88713dc4b77
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2018-12-13 13:12:56 +1100

    Make camera only movement
    commit die

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function startGame() {
         enemys[i] = new gameObject(30, 30, "blue", Math.floor(Math.random() * (1370 - -1370) ) + -1370, Math.floor(Math.random() * (1370 - -1370) ) + -1370);
         enemysball[i] = new gameObject(10, 10, "white", enemys[i].x, enemys[i].y);
     }
-    player = new gameObject(window.innerWidth - 10, window.innerHeight - 20, "red", 10, 120);
+    player = new gameObject(30, 30, "red", window.innerWidth - 10, window.innerHeight - 20);
     snowball = new gameObject(10, 10, "white", player.x,player.y);
     gameArea.start();
 }
