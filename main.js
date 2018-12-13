74f7b8434a508d8d054e6928e151f9600d61fefd
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2018-12-14 09:58:50 +1100

    Update main.js

--- main.js
+++ main.js
@@ -15,11 +15,11 @@ var mouY;
 
 function startGame() {
     for (var i = 0; i < enemis; i++) {
-        enemys[i] = new gameObject(30, 30, "blue", Math.floor(Math.random() * (1370 - -1370) ) + -1370, Math.floor(Math.random() * (1370 - -1370) ) + -1370);
-        enemysball[i] = new gameObject(10, 10, "white", enemys[i].x, enemys[i].y);
+        enemys[i] = new gameObject(30, 30, "Enemy.png", Math.floor(Math.random() * (1370 - -1370) ) + -1370, Math.floor(Math.random() * (1370 - -1370) ) + -1370);
+        //enemysball[i] = new gameObject(10, 10, "white", enemys[i].x, enemys[i].y);
     }
-    player = new gameObject(30, 30, "red", (window.innerWidth - 10)/2, (window.innerHeight - 20)/2);
-    snowball = new gameObject(10, 10, "white", player.x,player.y);
+    player = new gameObject(30, 30, "Player.png", (window.innerWidth - 10)/2, (window.innerHeight - 20)/2);
+    snowball = new gameObject(10, 10, "Player.png", player.x,player.y);
     gameArea.start();
     for (var i = -1370; i < 1370; i+=10) {
         oogboog[i] = new gameObject(10, 10, "blue", -1370, i);
@@ -83,7 +83,8 @@ function gameObject(width, height, color, x, y) {
             ctx.translate(this.x, this.y); 
             ctx.rotate(this.angle);
             ctx.fillStyle = color;
-            ctx.fillRect(this.width / -2, this.height / -2, this.width, this.height); 
+            this.image = new Image();
+            this.image.src = color;
             //ctx.fillRect(this.width / -2, this.height / -2, this.width, this.height); 
             ctx.restore(); 
         }
