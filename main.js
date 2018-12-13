e2aff8ec1f80e78fe538e34e78cdc481bf31b454
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2018-12-13 13:14:30 +1100

    fix the position of the player

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function startGame() {
         enemys[i] = new gameObject(30, 30, "blue", Math.floor(Math.random() * (1370 - -1370) ) + -1370, Math.floor(Math.random() * (1370 - -1370) ) + -1370);
         enemysball[i] = new gameObject(10, 10, "white", enemys[i].x, enemys[i].y);
     }
-    player = new gameObject(30, 30, "red", window.innerWidth - 10, window.innerHeight - 20);
+    player = new gameObject(30, 30, "red", (window.innerWidth - 10)/2, (window.innerHeight - 20)/2);
     snowball = new gameObject(10, 10, "white", player.x,player.y);
     gameArea.start();
 }
