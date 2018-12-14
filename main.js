226a9cdad746bcc49aa791c0886b1ae133b0763d
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2018-12-14 13:07:54 +1100

    Update main.js

--- main.js
+++ main.js
@@ -83,7 +83,8 @@ function gameObject(width, height, color, x, y) {
             ctx.save();
             ctx.fillStyle = "green";
             ctx.font = "30pt Calibri";
-            ctx.fillText(prevCore, 150, 50);
+            ctx.fillText("Previous Scores:", 150, 50);
+            ctx.fillText(prevCore, 250, 50);
             ctx.fillText(core, 100, 50);
             ctx.translate(this.x, this.y); 
             ctx.rotate(this.angle);
